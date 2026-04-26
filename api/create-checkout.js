e1203a075dd14dac444cab4c0ecf1f881274ec7a
marcmasterbet <marc.masterbet@gmail.com>
2026-04-26 20:12:50 +0200

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,6 +1,6 @@
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
